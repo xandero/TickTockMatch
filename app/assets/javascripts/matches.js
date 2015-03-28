dd826f379a42b5486f27562a30fbcaea3762f531
xandero <alexander.ohan@gmail.com>
2015-03-28 15:22:30 +1100

    logic for javascript events

---
+++ app/assets/javascripts/matches.js
@@ -0,0 +1,43 @@
+
+create multiple div vertically down the page that each represent a conversation.
+For each match do:
+
+Create small div on LHS with thumbnail photo of the person matched with.
+Display matches name at top of div.
+if both questions have been answered 
+   display the last message of the conversation within the div
+  else 
+    there are no messages, display "No messages...yet"
+  end
+else if one or more questions not answered
+  if the viewer has not answered the question
+    show "your question awaits!" (also if neither have answered)
+  else
+    the person matched has not answered, show "awaiting answer from your match"
+  end 
+end
+
+On click of each div, 
+  if both questions have been answered
+    load the relevant conversation page.
+  else
+    load the questions page
+  end
+
+
+
+
+
+
+function addConversation () {
+    var NewContent = '<div class="reply"><input name="name" type="text" id="name" size="20" value="" style="height:20px; margin-top:10px; width:480px;margin-left:90px; font-size:14px;" /></div>'
+    $('.').on('click', function () {
+        var $this = $(this);
+
+        if ($reply.length) {
+            $reply.toggle();
+        } else {
+            $(NewContent).insertAfter($this);
+        }
+    });
+};
---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,10 @@
+create two buttons at the top of page "#my-question", "#their-question"
+
+create a div #question, within which the question text will be placed.
+create a div #answer, in which answers will be placed (if they exist)
+create an event handler:
+  on 'click' #my-question
+    load the current user question into the question div
+    if the reciprocator has answered
+      load their answer into #answer
+     
