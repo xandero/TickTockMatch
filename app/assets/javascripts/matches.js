554d5af7417a0d141bc4d4c5272f7c957c52a2e1
xandero <alexander.ohan@gmail.com>
2015-03-29 11:23:23 +1100

    updates to profilesJS

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -2,33 +2,33 @@ For each match do:
   create div across page with 'match-id' as the div ID or a data field?
   (is it possible to sort these divs by descending order of @match.last_updated ?)
 
-Create small div on LHS with thumbnail photo of the person matched with.
-Display matches name at top of div.
+Determine whether @current_user id user1 or user2 in the match model.
+  then set var thisUser = user1_id/user2_id
+  also set var matchedUser = user1_id/user2_id
+
+Create small div on LHS with thumbnail photo of the matchedUser.
+Display matchedUser name at top of div.
 if both questions have been answered 
    display the last message of the conversation within the div
   else 
     there are no messages, display "No messages...yet"
   end
 else if one or more questions not answered
-  if the viewer has not answered the question
-    show "your question awaits!" (also if neither have answered)
+  if thisUser has not answered the question
+    show "your question awaits!" (also show this message if neither have answered)
   else
-    the person matched has not answered, show "awaiting answer from your match"
+    the matchedUser has not answered, show "awaiting answer from your match"
   end 
 end
 
 Create event handler
 
-On click of each div, 
+On 'click' of each match div, 
   if conversation with match_id (for this div, which will be either its ID or in its data field) exists
     load conversation.js
   else
     load questions.js
   end
-
-
-
-
 
 
 function addConversation () {
--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,6 +3,11 @@ class MatchesController < ApplicationController
   def determine_user
     find id of current_user
     need to figure out whether @current_user is user1 or user2 in each match
+
+    find each match with @current_user in either user1 or user2 position
+    
+
+
   end
 
 end
